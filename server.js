e4790dfc5e8e209dc227a06a25728b580aab74e2
lewy192 <lewis.james.hill@outlook.com>
2021-05-09 18:46:24 +1000

    server getting and posting work, when note is clicked on the text content is now displayed in the right column

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
-const Express = require("express");
+const express = require("express");
 const fs = require("fs");
-const app = Express();
+const app = express();
+
+// db import
+const db = require("./db/db.json");
 
 const PORT = 8080;
+
+app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`);
 });
 
-app.get("/post", (req, res) => {});
+app.get("/notes", (req, res) => {
+    res.sendFile(`${__dirname}/public/notes.html`);
+});
+
+// api calls
+
+app.get("/api/notes", (req, res) => {
+    res.json(db);
+});
+
+app.post("/api/notes", (req, res) => {
+    let notesJson = fs.readFileSync("./db/db.json");
+    let notes = JSON.parse(notesJson);
+    notes.push(req.body);
+    notesJson = JSON.stringify(notes);
+
+    fs.writeFileSync("./db/db.json", notesJson);
+});
 
 app.listen(PORT, () => {
     console.log(`listening on port" ${PORT}`);
